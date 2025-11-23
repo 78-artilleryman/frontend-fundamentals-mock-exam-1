0169cb98993f0d06c8f6de7e4295858bdc04318e
Byeonghyeon Yun <86054169+78-artilleryman@users.noreply.github.com>
2025-11-23 14:28:30 +0900

    feat: 적금 계산기 입력 기능 구현

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -4,6 +4,9 @@ import { getProducts, Product } from '../services/products';
 
 export function SavingsCalculatorPage() {
   const [products, setProducts] = useState<Product[]>([]);
+  const [goalAmount, setGoalAmount] = useState('');
+  const [monthlyAmount, setMonthlyAmount] = useState('');
+  const [savingTerm, setSavingTerm] = useState(12);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,17 +24,35 @@ export function SavingsCalculatorPage() {
       setProducts([]);
     };
   }, []);
+
   return (
     <>
       <NavigationBar title="적금 계산기" />
 
       <Spacing size={16} />
 
-      <TextField label="목표 금액" placeholder="목표 금액을 입력하세요" suffix="원" />
+      <TextField
+        label="목표 금액"
+        placeholder="목표 금액을 입력하세요"
+        suffix="원"
+        value={goalAmount}
+        onChange={e => setGoalAmount(e.target.value)}
+      />
       <Spacing size={16} />
-      <TextField label="월 납입액" placeholder="희망 월 납입액을 입력하세요" suffix="원" />
+      <TextField
+        label="월 납입액"
+        placeholder="희망 월 납입액을 입력하세요"
+        suffix="원"
+        value={monthlyAmount}
+        onChange={e => setMonthlyAmount(e.target.value)}
+      />
       <Spacing size={16} />
-      <SelectBottomSheet label="저축 기간" title="저축 기간을 선택해주세요" value={12} onChange={() => {}}>
+      <SelectBottomSheet
+        label="저축 기간"
+        title="저축 기간을 선택해주세요"
+        value={savingTerm}
+        onChange={value => setSavingTerm(value)}
+      >
         <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
         <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
         <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
